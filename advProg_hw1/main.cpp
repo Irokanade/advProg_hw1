291d55a6d123929cbc714f24c941db425d3ca53b
Irokanade <michaeljs.leong@gmail.com>
2023-03-10 17:07:18 +0800

    Initial Commit

---
+++ advProg_hw1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  advProg_hw1
+//
+//  Created by Michael Leong on 10/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
